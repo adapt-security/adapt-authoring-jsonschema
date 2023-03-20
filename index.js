c7ddabea18e1df00c532a55403530de6fd1bad1a
Thomas Taylor <hello@tomtaylor.name>
2023-03-20 15:06:03 +0000

    Enable eslint and standard ruleset

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/** 
+/**
  * Validation of data against JSON schemas
  * @namespace jsonschema
  */
-export { default } from './lib/JsonSchemaModule.js';
+export { default } from './lib/JsonSchemaModule.js'
